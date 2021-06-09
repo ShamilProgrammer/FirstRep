e38fa31aa972dcbed29edc67be2d913ffe581721
magomedovsm <magomedovsm@SQL3TSRV>
2021-06-09 12:50:46 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication6/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pivotGridControl1 = new DevExpress.XtraPivotGrid.PivotGridControl();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pivotGridControl1
+            // 
+            this.pivotGridControl1.ActiveFilterString = "";
+            this.pivotGridControl1.Location = new System.Drawing.Point(12, 60);
+            this.pivotGridControl1.Name = "pivotGridControl1";
+            this.pivotGridControl1.Size = new System.Drawing.Size(495, 392);
+            this.pivotGridControl1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 464);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pivotGridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraPivotGrid.PivotGridControl pivotGridControl1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using DevExpress.XtraPivotGrid;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=(localDB)\MSSQLLocalDB;Initial Catalog=ShamilTestDB;Integrated Security=True";
+
+        SqlConnection sqlConnection = null;
+
+        DataSet set;
+
+        public Form1()
+        {
+            InitializeComponent();
+            sqlConnection = new SqlConnection(connectionString);
+            pivotGridControl1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Bottom;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            set = new DataSet();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("Select * from employees", sqlConnection);
+
+            adapter.Fill(set);
+
+            pivotGridControl1.DataSource = set.Tables[0];
+
+            SetData(set.Tables[0]);
+        }
+
+        public void SetData(DataTable table)
+        {
+            foreach(DataColumn column in set.Tables[0].Columns)
+            {
+                PivotGridField field = new PivotGridField(column.ColumnName, PivotArea.FilterArea);
+                pivotGridControl1.Fields.Add(field);
+            }
+        }
+        
+    }
+}
